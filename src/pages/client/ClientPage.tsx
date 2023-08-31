cd3bafdd4f009b6532102ff7743358c0b3e58414
Filip Slezak <fslezak@teonite.com>
2023-08-31 14:18:14 +0200

    working enrollment

---
+++ src/pages/client/ClientPage.tsx
@@ -0,0 +1,6 @@
+import './style.scss';
+
+import { PageContainer } from '../../shared/components/layout/PageContainer/PageContainer';
+export const ClientPage = () => {
+  return <PageContainer id="client-page"></PageContainer>;
+};
