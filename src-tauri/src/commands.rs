2e9acded2e9a7ac12bf8d579bc55e7e23bae71ae
dzania <artur.kantorczyk@gmail.com>
2023-09-14 15:52:00 +0200

    add remove interface command

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,10 +1,19 @@
 use crate::{database::Location, error::Error, utils::setup_interface, AppState};
 use tauri::State;
+use wireguard_rs::netlink::delete_interface;
 
 // Create new wireguard interface
 pub async fn connect(location_id: i64, app_state: State<'_, AppState>) -> Result<(), Error> {
     if let Some(location) = Location::find_by_id(&app_state.get_pool(), location_id).await? {
         setup_interface(location, &app_state.get_pool()).await?;
+    }
+    Ok(())
+}
+
+// Create new wireguard interface
+pub async fn disconnect(location_id: i64, app_state: State<'_, AppState>) -> Result<(), Error> {
+    if let Some(location) = Location::find_by_id(&app_state.get_pool(), location_id).await? {
+        delete_interface(&location.name)?;
     }
     Ok(())
 }
