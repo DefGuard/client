a9aaa548b3a11e3025211ea0a4f15f51ee2acfd1
dzania <artur.kantorczyk@gmail.com>
2023-10-04 14:29:05 +0200

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { attachConsole } from 'tauri-plugin-log-api';
 
 import { App } from './components/App/App';
 import { ToastManager } from './shared/defguard-ui/components/Layout/ToastManager/ToastManager';
-import { attachConsole } from 'tauri-plugin-log-api';
 
 // Attach console for logging
 attachConsole();
