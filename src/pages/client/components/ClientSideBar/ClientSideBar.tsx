ae0c8a1cfda08b42e2b1050f13577e77a0f12247
dzania <artur.kantorczyk@gmail.com>
2023-10-11 12:53:20 +0200

    wip responsive

--- src/pages/client/components/ClientSideBar/ClientSideBar.tsx
+++ src/pages/client/components/ClientSideBar/ClientSideBar.tsx
@@ -1,20 +1,46 @@
 import './style.scss';
 
+import { useBreakpoint } from 'use-breakpoint';
+
 import { useI18nContext } from '../../../../i18n/i18n-react';
+import SvgDefguardLogoCollapsed from '../../../../shared/components/svg/DefguardLogoCollapsed';
 import SvgDefguardLogoIcon from '../../../../shared/components/svg/DefguardLogoIcon';
 import SvgDefguardLogoText from '../../../../shared/components/svg/DefguardLogoText';
 import SvgIconNavConnections from '../../../../shared/components/svg/IconNavConnections';
+import { deviceBreakpoints } from '../../../../shared/constants';
 import { IconContainer } from '../../../../shared/defguard-ui/components/Layout/IconContainer/IconContainer';
 import SvgIconPlus from '../../../../shared/defguard-ui/components/svg/IconPlus';
 import { useClientStore } from '../../hooks/useClientStore';
 import { useAddInstanceModal } from '../modals/AddInstanceModal/hooks/useAddInstanceModal';
 import { ClientBarItem } from './components/ClientBarItem/ClientBarItem';
 
-export const ClientSideBar = () => {
-  const { LL } = useI18nContext();
+const ClientSideBarMobile = () => {
   const instances = useClientStore((state) => state.instances);
 
   return (
+    <div id="client-page-side">
+      <div className="logo">
+        <SvgDefguardLogoCollapsed />
+      </div>
+      <div className="items">
+        <div className="client-bar-item active">
+          <SvgIconNavConnections />
+        </div>
+        {instances.map((instance) => (
+          <ClientBarItem instance={instance} key={instance.id} />
+        ))}
+        <AddInstance />
+      </div>
+    </div>
+  );
+};
+
+export const ClientSideBar = () => {
+  const { LL } = useI18nContext();
+  const instances = useClientStore((state) => state.instances);
+  const { breakpoint } = useBreakpoint(deviceBreakpoints);
+
+  return breakpoint === 'desktop' ? (
     <div id="client-page-side">
       <div className="logo">
         <SvgDefguardLogoIcon />
@@ -31,18 +57,21 @@ export const ClientSideBar = () => {
         <AddInstance />
       </div>
     </div>
+  ) : (
+    <ClientSideBarMobile />
   );
 };
 
 const AddInstance = () => {
   const { LL } = useI18nContext();
   const openAddInstanceModal = useAddInstanceModal((state) => state.open);
+  const { breakpoint } = useBreakpoint(deviceBreakpoints);
   return (
     <div className="client-bar-item clickable" onClick={() => openAddInstanceModal()}>
       <IconContainer>
         <SvgIconPlus />
       </IconContainer>
-      <p>{LL.pages.client.sideBar.addInstance()}</p>
+      {breakpoint === 'desktop' && <p>{LL.pages.client.sideBar.addInstance()}</p>}
     </div>
   );
 };
