b953441b390ded162fdfe1f02cd40b3774378cd5
Maciej Wójcik <wojcik91@gmail.com>
2023-10-16 09:31:36 +0200

    add docstrings

--- src-tauri/src/bin/defguard-client.rs
+++ src-tauri/src/bin/defguard-client.rs
@@ -1,3 +1,5 @@
+//! defguard desktop client
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
--- src-tauri/src/bin/defguard-service.rs
+++ src-tauri/src/bin/defguard-service.rs
@@ -1,3 +1,8 @@
+//! defguard interface management daemon
+//!
+//! This binary is meant to run as a daemon with root privileges
+//! and communicate with the desktop client over HTTP.
+
 use defguard_client::service::run_server;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
