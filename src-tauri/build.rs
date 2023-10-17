925e7aa8f4c0f23e2de58273d6f3decf0e0dbf6e
Maciej Wójcik <wojcik91@gmail.com>
2023-10-17 08:15:46 +0200

    grpn initial setup

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,15 @@
-fn main() {
-    tauri_build::build()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tauri_build::build();
+    // compiling protos using path on build time
+    let mut config = prost_build::Config::new();
+    // enable optional fields
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    // Make all messages serde-serializable
+    config.type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]");
+    tonic_build::configure().compile_with_config(
+        config,
+        &["proto/client/client.proto"],
+        &["proto/client"],
+    )?;
+    Ok(())
 }
