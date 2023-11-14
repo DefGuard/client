f1c3efb815ac63bb072f313a7c6a9e3af44d730c
Kamil Chudy <kchudy@teonite.com>
2023-11-14 10:55:52 +0100

    feat: added macOS PKG build process (#67)
    Added installation script
    
    Added resources
    
    Added macOS bundle postscript support
    
    Added build target
    
    Installed target for macOS
    
    Added protobuf installer for macOS
    
    Refactored build jobs
    
    Added macos artifacts
    
    Added product signing
    
    Fixed workflow
    
    Added keychain for singing
    
    Added keychain auth
    
    Renemed variable
    
    Refactored workflow
    
    Added notary process support
    
    Added keychain unlock step
    
    Added Apple certificate
    
    Added verbose output
    
    Added building wireguard-go
    
    Made wireguard-go executable
    
    Changed file permissions
    
    Refactored workflow
    
    Update src-tauri/src/utils.rs
    
    Co-authored-by: Adam <adam@NetBSD.org>
    
    Update src-tauri/resources-macos/scripts/postinstall
    
    Co-authored-by: Adam <adam@NetBSD.org>
    
    Added forced symlinks

---
+++ src-tauri/resources-macos/resources/uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SERVICE_BINARY=defguard-service
+DAEMON_PROPERTY_FILE=net.defguard.plist
+WIREGUARD_GO_BINARY=wireguard-go
+DAEMON_NAME=net.defguard
+PACKAGE_ID=net.defguard
+
+#Check running user
+if (( $EUID != 0 )); then
+    echo "Please run as root."
+    exit
+fi
+
+# Remove wireguard-go shortcut at /usr/local/bin
+rm -f /usr/local/bin/${WIREGUARD_GO_BINARY}
+
+# Remove service shortcut at /usr/local/bin
+rm -f /usr/local/bin/${SERVICE_BINARY}
+
+# Remove daemon
+launchctl stop ${DAEMON_NAME}
+launchctl unload /Library/LaunchDaemons/${DAEMON_PROPERTY_FILE}
+rm -f /Library/LaunchDaemons/${DAEMON_PROPERTY_FILE}
+
+pkgutil --forget ${PACKAGE_ID} > /dev/null 2>&1
+
+rm -rf /Applications/defguard-client.app
+
+echo "Application uninstall process finished"
