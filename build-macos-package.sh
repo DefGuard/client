772e8cc0c726fcd70db49f5c567096a8e4843885
Kamil Chudy <kchudy@teonite.com>
2023-11-23 10:09:26 +0100

    Added notary process support

--- build-macos-package.sh
+++ build-macos-package.sh
@@ -4,7 +4,7 @@ set -e
 
 TARGET_DIRECTORY=$1
 SCRIPTS_DIRECTORY=$2
-DEVELOPER_ID_INSTALLER=$3
+APPLE_PACKAGE_SIGNING_IDENTITY=$3
 KEYCHAIN=$4
 
 mkdir -p "${TARGET_DIRECTORY}/package"
@@ -21,7 +21,7 @@ pkgbuild \
 PACKAGE_PATH="${TARGET_DIRECTORY}/package/defguard.pkg"
 
 pkgbuild \
-    --identifier net.defguard \
+    --identifier "net.defguard" \
     --root ${APP_ROOT} \
     --component-plist ${TARGET_DIRECTORY}/defguard-client.plist \
     --install-location "/Applications/defguard-client.app" \
@@ -33,7 +33,7 @@ productbuild \
     "${TARGET_DIRECTORY}/product/defguard.pkg"
 
 productsign \
-    --sign "Developer ID Installer: ${DEVELOPER_ID_INSTALLER}" \
-    --keychain ${KEYCHAIN} \
+    --sign "${APPLE_PACKAGE_SIGNING_IDENTITY}" \
+    --keychain "${KEYCHAIN}" \
     "${TARGET_DIRECTORY}/product/defguard.pkg" \
     "${TARGET_DIRECTORY}/product-signed/defguard.pkg"
