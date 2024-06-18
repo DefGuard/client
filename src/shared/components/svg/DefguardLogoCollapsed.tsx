7e0e3e35a0f303ed2969c00f7443b311b9f7c217
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2024-06-18 14:44:34 +0200

    feat: add new logo (#241)
    * logo update
    
    * add icons 1
    
    * add icons 2
    
    * fix linter error
    
    * update collapsed logo
    
    * fix typo

--- src/shared/components/svg/DefguardLogoCollapsed.tsx
+++ src/shared/components/svg/DefguardLogoCollapsed.tsx
@@ -1,38 +1,24 @@
 import type { SVGProps } from 'react';
-const SvgDefguadNavLogoCollapsed = (props: SVGProps<SVGSVGElement>) => (
+const SvgDefguardLogoCollapsed = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={20.863}
-    height={44}
-    viewBox="0 0 20.863 44"
+    width={21}
+    height={43}
+    fill="none"
+    viewBox="0 0 21 43"
     {...props}
   >
+    <g clipPath="url(#defguard-logo-collapsed_svg__a)">
+      <path
+        fill="#0C8CE0"
+        d="M18.706 0v9.315l-8.178-4.658L0 10.65v22.625l10.52 5.992 8.178-4.658v4.8l-3.957 2.258L17.08 43l3.957-2.257V21.29L10.52 15.298 2.34 19.956v-7.98l8.18-4.658 8.178 4.657v2.652l2.34 1.334V1.334L18.698 0zM2.34 31.94v-7.981l8.18 4.657 8.178-4.657v7.98l-8.179 4.658zm15.192-9.978-7.013 3.994-7.013-3.994 7.013-3.995z"
+      />
+    </g>
     <defs>
-      <linearGradient
-        id="defguad-nav-logo-collapsed_svg__a"
-        x1={0.5}
-        x2={0.5}
-        y2={1}
-        gradientUnits="objectBoundingBox"
-      >
-        <stop offset={0} stopColor="#2accff" />
-        <stop offset={1} stopColor="#0071d4" />
-      </linearGradient>
+      <clipPath id="defguard-logo-collapsed_svg__a">
+        <path fill="#fff" d="M0 0h21v43H0z" />
+      </clipPath>
     </defs>
-    <path
-      d="m1710.288-1889.281-3.477-2.007-3.477 2.007 3.477 2.008Z"
-      style={{
-        fill: 'none',
-      }}
-      transform="translate(-1696.379 1911.257)"
-    />
-    <path
-      d="M1660.863-2055.789v-15.961l-3.477-2v7.965l-6.954-4-10.431 6v24.018l10.431 6 6.954-4v4.015l-3.473 2 3.477 2 3.473-2v-20.032l-10.431-6-6.954 4v-8.01l6.954-4 6.954 4v4Zm-10.431 2.012 3.477 2-3.477 2-3.477-2Zm0 8 6.954-4v8.006l-6.954 4-6.954-4v-8.006Z"
-      style={{
-        fill: 'url(#defguad-nav-logo-collapsed_svg__a)',
-      }}
-      transform="translate(-1640 2073.751)"
-    />
   </svg>
 );
-export default SvgDefguadNavLogoCollapsed;
+export default SvgDefguardLogoCollapsed;
