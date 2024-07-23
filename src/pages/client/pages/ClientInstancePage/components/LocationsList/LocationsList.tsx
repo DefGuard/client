a7e3270044a4b9811c9f0bd98e2173960fe6308f
cpprian <cpprian456@gmail.com>
2024-07-23 16:17:00 +0200

    fix tunnel pages

--- src/pages/client/pages/ClientInstancePage/components/LocationsList/LocationsList.tsx
+++ src/pages/client/pages/ClientInstancePage/components/LocationsList/LocationsList.tsx
@@ -5,33 +5,25 @@ import { useI18nContext } from '../../../../../../i18n/i18n-react';
 import { useToaster } from '../../../../../../shared/defguard-ui/hooks/toasts/useToaster';
 import { routes } from '../../../../../../shared/routes';
 import { useClientStore } from '../../../../hooks/useClientStore';
-import {
-  CommonWireguardFields,
-  DefguardInstance,
-  WireguardInstanceType,
-} from '../../../../types';
+import { CommonWireguardFields, WireguardInstanceType } from '../../../../types';
 import { LocationsDetailView } from './components/LocationsDetailView/LocationsDetailView';
 import { LocationsGridView } from './components/LocationsGridView/LocationsGridView';
 import { MFAModal } from './modals/MFAModal/MFAModal';
 
 interface LocationsListProps {
-  selectedInstance: DefguardInstance | undefined;
-  selectedInstanceType: WireguardInstanceType | undefined;
   locations: CommonWireguardFields[] | undefined;
   isError: boolean;
 }
 
-export const LocationsList = ({
-  selectedInstance,
-  selectedInstanceType,
-  locations,
-  isError,
-}: LocationsListProps) => {
+export const LocationsList = ({ locations, isError }: LocationsListProps) => {
   const { LL } = useI18nContext();
 
   const selectedView = useClientStore((state) => state.settings.selected_view);
+  const selectedInstance = useClientStore((state) => state.selectedInstance);
   const toaster = useToaster();
   const navigate = useNavigate();
+
+  const isTunnelType = selectedInstance?.type === WireguardInstanceType.TUNNEL;
 
   useEffect(() => {
     if (isError) {
@@ -42,31 +34,31 @@ export const LocationsList = ({
   useEffect(() => {
     if (
       locations?.length === 0 &&
-      selectedInstanceType === WireguardInstanceType.TUNNEL
+      selectedInstance?.type === WireguardInstanceType.TUNNEL
     ) {
       navigate(routes.client.addTunnel, { replace: true });
     }
-  }, [locations, navigate, selectedInstanceType]);
+  }, [locations, navigate, selectedInstance]);
 
   // TODO: add loader or another placeholder view pointing to opening enter token modal if no instances are found / present
   if (!selectedInstance || !locations) return null;
 
   return (
     <>
-      {locations.length === 1 && selectedView === null && (
+      {locations.length === 1 && selectedView === null && !isTunnelType && (
         <LocationsDetailView
           locations={locations}
-          connectionType={selectedInstanceType}
+          connectionType={selectedInstance?.type}
         />
       )}
-      {(selectedView === 'grid' || selectedView === null) && (
+      {(selectedView === 'grid' || selectedView === null || isTunnelType) && (
         <LocationsGridView locations={locations} />
       )}
 
-      {selectedView === 'detail' && (
+      {selectedView === 'detail' && !isTunnelType && (
         <LocationsDetailView
           locations={locations}
-          connectionType={selectedInstanceType}
+          connectionType={selectedInstance.type}
         />
       )}
 
