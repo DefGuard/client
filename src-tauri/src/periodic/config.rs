9a360205e2b88842ad3be5e04e7641eaddf5d9b8
Jacek Chmielewski <jchmielewski@teonite.com>
2024-09-03 14:55:43 +0200

    Cleanup: error handling, remove unwraps

--- src-tauri/src/periodic/config.rs
+++ src-tauri/src/periodic/config.rs
@@ -1,7 +1,6 @@
 use std::{collections::HashSet, time::Duration};
 use tauri::{AppHandle, Manager, State};
 use tokio::time::sleep;
-use tracing::warn;
 
 use crate::{
     appstate::AppState,
@@ -12,20 +11,24 @@ use crate::{
     },
     error::Error,
     events::{CONFIG_CHANGED, INSTANCE_UPDATE},
-    proto::{InstanceInfoRequest, InstanceInfoResponse},
+    proto::{DeviceConfigResponse, InstanceInfoRequest, InstanceInfoResponse},
 };
 
 const INTERVAL_SECONDS: Duration = Duration::from_secs(30);
-const POLLING_ENDPOINT: &str = "api/v1/poll";
+const POLLING_ENDPOINT: &str = "/api/v1/poll";
 
 /// Periodically retrieves and updates configuration for all [`Instance`]s.
-/// Updates are only performed if no connections are established for the [`Instance`].
+/// Updates are only performed if no connections are established to the [`Instance`],
+/// otherwise UI message is displayed.
 pub async fn poll_config(handle: AppHandle) {
     let state: State<AppState> = handle.state();
     let pool = state.get_pool();
     loop {
-        // TODO(jck): unwrap
-        let instances = Instance::all(&pool).await.unwrap();
+        let Ok(instances) = Instance::all(&pool).await else {
+            error!("Failed to retireve instances, retrying in {}s", INTERVAL_SECONDS.as_secs());
+            sleep(INTERVAL_SECONDS).await;
+            continue;
+        };
         debug!(
             "Polling configuration updates for {} instances",
             instances.len(),
@@ -33,7 +36,7 @@ pub async fn poll_config(handle: AppHandle) {
         for instance in &instances {
             if let Err(err) = poll_instance(&pool, instance, handle.clone()).await {
                 error!(
-                    "Failed to retrieve instance {}({}) config, {}",
+                    "Failed to retrieve instance {}({}) config: {}",
                     instance.name, instance.id, err
                 );
             } else {
@@ -59,105 +62,107 @@ pub async fn poll_instance(
     instance: &Instance<Id>,
     handle: AppHandle,
 ) -> Result<(), Error> {
-    // TODO(jck): unwrap
-    let WireguardKeys { pubkey, .. } = WireguardKeys::find_by_instance_id(pool, instance.id)
-        .await?
-        .unwrap();
+    // Query proxy api
+    let request = build_request(pool, instance).await?;
     let url = format!("{}{}", instance.proxy_url, POLLING_ENDPOINT);
-    let token = if let Some(token) = &instance.token {
-        token.to_string()
+    let response = reqwest::Client::new().post(url).json(&request).send().await;
+    let response = response.map_err(|err| {
+        Error::InternalError(format!(
+            "HTTP request failed for instance {}({}), url: {}, {}",
+            instance.name, instance.id, instance.proxy_url, err
+        ))
+    })?;
+    debug!("InstanceInfoResponse: {:?}", response);
+
+    // Parse the response
+    let response: InstanceInfoResponse = response.json().await.map_err(|err| {
+        Error::InternalError(format!(
+            "Failed to parse InstanceInfoResponse for instance {}({}): {}",
+            instance.name, instance.id, err,
+        ))
+    })?;
+    let device_config = response
+        .device_config
+        .as_ref()
+        .ok_or_else(|| Error::InternalError("Device config not present in response".to_string()))?;
+
+    // Early return if config didn't change
+    if !config_changed(pool, instance, device_config).await? {
+        return Ok(());
+    }
+
+    // Config changed. If there are no active connections for this instance, update the database.
+    // Otherwise just display a message to reconnect.
+    let state: State<'_, AppState> = handle.state();
+    if state.active_connections(instance).await?.is_empty() {
+        debug!(
+            "Updating instance {}({}) configuration: {device_config:?}",
+            instance.name, instance.id,
+        );
+        update_instance(instance.id, device_config.clone(), state, handle.clone()).await?;
+        handle.emit_all(INSTANCE_UPDATE, ())?;
+        info!(
+            "Updated instance {}({}) configuration",
+            instance.name, instance.id
+        );
     } else {
-        let msg = format!("Instance {} missing token, skipping", instance.name);
-        warn!(msg);
-        return Err(Error::InternalError(msg));
-    };
-    let client = reqwest::Client::new();
-    let request = InstanceInfoRequest {
-        token: token.clone(),
-        pubkey,
-    };
-    let response = client.post(url).json(&request).send().await;
-    if let Ok(response) = response {
-        info!("Reponse: {:#?}", response);
-        // TODO(jck): unwrap
-        let response: InstanceInfoResponse = response.json().await.unwrap();
-        // TODO(jck): unwraps
-        let instance = Instance::find_by_uuid(
-            pool,
-            &response
-                .device_config
-                .as_ref()
-                .unwrap()
-                .instance
-                .as_ref()
-                .unwrap()
-                .id,
-        )
-        // TODO(jck): unwrap
+        debug!(
+            "Emitting config-changed event for instance {}({})",
+            instance.name, instance.id,
+        );
+        let _ = handle.emit_all(CONFIG_CHANGED, &instance.name);
+        info!(
+            "Emitted config-changed event for instance {}({})",
+            instance.name, instance.id,
+        );
+    }
+
+    Ok(())
+}
+
+/// Returns true if configuration in instance_info differs from current configuration
+async fn config_changed(
+    pool: &DbPool,
+    instance: &Instance<Id>,
+    device_config: &DeviceConfigResponse,
+) -> Result<bool, Error> {
+    let db_locations: Vec<Location<NoId>> = Location::find_by_instance_id(pool, instance.id)
         .await?
-        .unwrap();
-        let db_locations = Location::find_by_instance_id(pool, instance.id).await?;
-        // let db_comparable_locations: Vec<ComparableLocation> = db_locations
-        let db_comparable_locations: Vec<Location<NoId>> = db_locations
-            .into_iter()
-            .map(Location::<NoId>::from)
-            .collect();
-        let db_comparable_locations: HashSet<Location<NoId>> =
-            HashSet::from_iter(db_comparable_locations);
-        // TODO(jck): unwrap
-        let core_locations: Vec<Location<NoId>> = response
-            .device_config
-            .as_ref()
-            .unwrap()
-            .configs
-            .iter()
-            .map(|config| device_config_to_location(config.clone(), instance.id))
-            .map(Location::<NoId>::from)
-            .collect();
-        let core_comparable_locations: HashSet<Location<NoId>> = HashSet::from_iter(core_locations);
+        .into_iter()
+        .map(Location::<NoId>::from)
+        .collect();
+    let db_locations: HashSet<Location<NoId>> = HashSet::from_iter(db_locations);
+    let core_locations: Vec<Location<NoId>> = device_config
+        .configs
+        .iter()
+        .map(|config| device_config_to_location(config.clone(), instance.id))
+        .map(Location::<NoId>::from)
+        .collect();
+    let core_locations: HashSet<Location<NoId>> = HashSet::from_iter(core_locations);
 
-        if core_comparable_locations == db_comparable_locations {
-            // config remains unchanged, return early
-            return Ok(());
-        }
+    Ok(db_locations != core_locations)
+}
 
-        // Config changed
-
-        // If there are no active connections for this instance, update the database.
-        // Otherwise just display the message to reconnect.
-        let state: State<'_, AppState> = handle.state();
-        if state.active_connections(&instance).await?.is_empty() {
-            // TODO(jck): unwrap
-            debug!(
-                "Updating instance {}({}) configuration: {:?}",
-                instance.name, instance.id, response.device_config,
-            );
-            update_instance(
-                instance.id,
-                response.device_config.unwrap(),
-                state,
-                handle.clone(),
-            )
-            .await?;
-            handle.emit_all(INSTANCE_UPDATE, ())?;
-            info!(
-                "Updated instance {}({}) configuration",
+/// Retrieves pubkey & token to build InstanceInfoRequest
+async fn build_request(
+    pool: &DbPool,
+    instance: &Instance<Id>,
+) -> Result<InstanceInfoRequest, Error> {
+    let Some(WireguardKeys { pubkey, .. }) =
+        WireguardKeys::find_by_instance_id(pool, instance.id).await? else {
+            error!(
+                "WireguardKeys for instance {}({}) not found",
                 instance.name, instance.id
             );
-        } else {
-            debug!(
-                "Emitting config-changed event for instance {}({})",
-                instance.name, instance.id,
-            );
-            let _ = handle.emit_all(CONFIG_CHANGED, &instance.name);
-            info!(
-                "Emitted config-changed event for instance {}({})",
-                instance.name, instance.id,
-            );
-        }
-
-        Ok(())
-    } else {
-        Err(Error::InternalError("TODO(jck)".to_string()))
-    }
+            return Err(Error::NotFound);
+        };
+    let Some(token) = &instance.token else {
+        let msg = format!("Instance {} missing token", instance.name);
+        error!("{msg}");
+        return Err(Error::InternalError(msg));
+    };
+    Ok(InstanceInfoRequest {
+        token: token.clone(),
+        pubkey,
+    })
 }
