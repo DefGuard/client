72b16f129e2bed21cab386beaf2c3ae64ba1ac24
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2024-11-15 15:46:07 +0100

    Update src-tauri/src/periodic/connection.rs
    Co-authored-by: Aleksander <170264518+t-aleksander@users.noreply.github.com>

--- src-tauri/src/periodic/connection.rs
+++ src-tauri/src/periodic/connection.rs
@@ -255,7 +255,7 @@ pub async fn verify_active_connections(app_handle: AppHandle) -> Result<(), Erro
                                     .await;
                                 }
                                 Err(e) => {
-                                    warn!("Attempt to reconnect the tunnel {}({}) cannot be made, tunnel was not found in db. Reason: {} , connection will be dropped instead.", con.interface_name, con.location_id, e.to_string());
+                                    warn!("Attempt to reconnect the tunnel {}({}) cannot be made, because of a database error. Error details: {} , connection will be dropped instead.", con.interface_name, con.location_id, e.to_string());
                                     disconnect_dead_connection(
                                         con.location_id,
                                         &con.interface_name,
