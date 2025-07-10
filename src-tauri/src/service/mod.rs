411c1f6d1f462e63c9e90ff5374f56fc08129abd
Maciek <19913370+wojcik91@users.noreply.github.com>
2025-07-10 10:25:07 +0200

    update socket path
    Co-authored-by: Adam <aciarcinski@teonite.com>

--- src-tauri/src/service/mod.rs
+++ src-tauri/src/service/mod.rs
@@ -54,7 +54,7 @@ const DAEMON_HTTP_PORT: u16 = 54127;
 #[cfg(windows)]
 pub(super) const DAEMON_BASE_URL: &str = "http://localhost:54127";
 
-pub(super) const DAEMON_SOCKET_PATH: &str = "/tmp/defguard.socket";
+pub(super) const DAEMON_SOCKET_PATH: &str = "/var/run/defguard.socket";
 
 #[derive(Error, Debug)]
 pub enum DaemonError {
