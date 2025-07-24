8898f744cc6320ea707bc64beca9a6c987fc6991
Maciej Wójcik <maciek@wjck.pl>
2025-07-24 12:55:25 +0200

    handle multiple addresses in location card

--- src/pages/client/pages/ClientInstancePage/components/LocationsList/components/LocationCardTitle/LocationCardTitle.tsx
+++ src/pages/client/pages/ClientInstancePage/components/LocationsList/components/LocationCardTitle/LocationCardTitle.tsx
@@ -1,9 +1,13 @@
 import './style.scss';
 
 import classNames from 'classnames';
+import { useMemo } from 'react';
 
 import { Badge } from '../../../../../../../../shared/defguard-ui/components/Layout/Badge/Badge';
 import { BadgeStyleVariant } from '../../../../../../../../shared/defguard-ui/components/Layout/Badge/types';
+import { FloatingMenu } from '../../../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenu';
+import { FloatingMenuProvider } from '../../../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenuProvider';
+import { FloatingMenuTrigger } from '../../../../../../../../shared/defguard-ui/components/Layout/FloatingMenu/FloatingMenuTrigger';
 import SvgIconConnection from '../../../../../../../../shared/defguard-ui/components/svg/IconConnection';
 import { CommonWireguardFields } from '../../../../../../types';
 
@@ -19,7 +23,40 @@ export const LocationCardTitle = ({ location }: Props) => {
     <div className={cn}>
       <SvgIconConnection />
       <span className="location-name">{location?.name}</span>
-      <Badge text={location?.address || ''} styleVariant={BadgeStyleVariant.STANDARD} />
+
+      <AddressBadge addresses={location?.address || ''} />
     </div>
+  );
+};
+
+type AddressBadgeProps = {
+  // comma-separated list of addresses
+  addresses: string;
+};
+
+const AddressBadge = ({ addresses: address }: AddressBadgeProps) => {
+  // split into separate addreses to show in tooltip
+  const addresses = useMemo(() => address.split(','), [address]);
+
+  return (
+    <FloatingMenuProvider placement="right" disabled={addresses.length === 0}>
+      <FloatingMenuTrigger asChild>
+        <div className="addresses-badge-container">
+          <Badge
+            className="client-addresses"
+            text={address}
+            styleVariant={BadgeStyleVariant.STANDARD}
+            // ref={containerRef}
+          />
+        </div>
+      </FloatingMenuTrigger>
+      <FloatingMenu>
+        <ul className="list-addresses-floating">
+          {addresses.map((d) => (
+            <li key={d}>{d}</li>
+          ))}
+        </ul>
+      </FloatingMenu>
+    </FloatingMenuProvider>
   );
 };
