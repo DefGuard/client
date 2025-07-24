d21370e73c46500f52913482449a16f0b8820c69
Maciej Wójcik <maciek@wjck.pl>
2025-07-24 16:01:40 +0200

    add clipboard translation

--- src/shared/hooks/useClipboard.ts
+++ src/shared/hooks/useClipboard.ts
@@ -1,8 +1,11 @@
 import { useCallback } from 'react';
 
+import { useI18nContext } from '../../i18n/i18n-react';
 import { useToaster } from '../defguard-ui/hooks/toasts/useToaster';
 
 export const useClipboard = () => {
+  const { LL } = useI18nContext();
+
   const toaster = useToaster();
 
   const writeToClipboard = useCallback(
@@ -13,17 +16,17 @@ export const useClipboard = () => {
           if (customMessage) {
             toaster.success(customMessage);
           } else {
-            toaster.success('Content copied to clipboard');
+            toaster.success(LL.common.messages.clipboard.success());
           }
         } catch (e) {
-          toaster.error('Writing to clipboard failed !');
+          toaster.error(LL.common.messages.clipboard.error());
           console.error(e);
         }
       } else {
-        toaster.warning('Cannot access clipboard in insecure contexts');
+        toaster.warning(LL.common.messages.insecureContext());
       }
     },
-    [toaster],
+    [LL.common.messages, toaster],
   );
 
   return {
