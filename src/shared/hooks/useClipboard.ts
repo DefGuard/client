64b82aa4b67be7ed06053069b0ee6d8323727d54
Filip Ślęzak <102536422+filipslezaklab@users.noreply.github.com>
2025-09-08 10:45:41 +0200

    fix clipboard hook (#553)
    * fix clipboard hook
    
    fix permissions for opening file dialogs
    
    * Update deny.toml
    
    * Update test.yml

--- src/shared/hooks/useClipboard.ts
+++ src/shared/hooks/useClipboard.ts
@@ -1,3 +1,4 @@
+import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 import { useCallback } from 'react';
 
 import { useI18nContext } from '../../i18n/i18n-react';
@@ -10,20 +11,16 @@ export const useClipboard = () => {
 
   const writeToClipboard = useCallback(
     async (content: string, customMessage?: string) => {
-      if (window.isSecureContext) {
-        try {
-          await navigator.clipboard.writeText(content);
-          if (customMessage) {
-            toaster.success(customMessage);
-          } else {
-            toaster.success(LL.common.messages.clipboard.success());
-          }
-        } catch (e) {
-          toaster.error(LL.common.messages.clipboard.error());
-          console.error(e);
+      try {
+        await writeText(content);
+        if (customMessage) {
+          toaster.success(customMessage);
+        } else {
+          toaster.success(LL.common.messages.clipboard.success());
         }
-      } else {
-        toaster.warning(LL.common.messages.insecureContext());
+      } catch (e) {
+        toaster.error(LL.common.messages.clipboard.error());
+        console.error(e);
       }
     },
     [LL.common.messages, toaster],
