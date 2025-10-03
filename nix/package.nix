218135d0ba05b13f5f5c4a0a936b6f411a480803
Maciej Wójcik <maciej@defguard.net>
2025-10-03 08:01:28 +0200

    bump version to 1.5.2

--- nix/package.nix
+++ nix/package.nix
@@ -6,7 +6,7 @@
   makeDesktopItem,
 }: let
   pname = "defguard-client";
-  version = "1.5.1"; # TODO: Get this from Cargo.toml or git
+  version = "1.5.2"; # TODO: Get this from Cargo.toml or git
 
   desktopItem = makeDesktopItem {
     name = pname;
