b0cb3c79bfd4a41f071be7bc981f2dd59fdd952b
Maciej Wójcik <maciej@defguard.net>
2025-12-10 15:35:52 +0100

    fix nix package setup

--- nix/package.nix
+++ nix/package.nix
@@ -68,9 +68,6 @@ in
 
     cargoDeps = rustPlatform.importCargoLock {
       lockFile = ../src-tauri/Cargo.lock;
-      outputHashes = {
-        "boringtun-0.6.0" = "sha256-UlgcnHAdrWm3S1v5T4W0froF4jJNqRAsfcVuI2EMSgk=";
-      };
     };
 
     # prefetch pnpm dependencies
@@ -83,7 +80,7 @@ in
         ;
 
       fetcherVersion = 2;
-      hash = "sha256-Qt4xC7BO7JZn236jXVe2VPAAFNnxdSJAvq+PYflW264=";
+      hash = "sha256-B71cUUBmTI2fQpKsP3Ejk1lpyvVdaniqs1UFP8+Jg9g=";
     };
 
     buildPhase = ''
