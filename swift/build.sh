fa83f54a6d3c3292a5f9503d34d3b7cbfe0d0087
Adam Ciarciński <adam@defguard.net>
2026-02-12 14:55:45 +0100

    Upgrade BoringTun bindings

--- swift/build.sh
+++ swift/build.sh
@@ -21,25 +21,25 @@ done
 
 mkdir -p target/universal/release
 lipo -create \
-    target/aarch64-apple-darwin/release/libboringtun.a \
-    target/x86_64-apple-darwin/release/libboringtun.a \
-    -output target/universal/release/libboringtun.a
+    target/aarch64-apple-darwin/release/libdefguard_boringtun.a \
+    target/x86_64-apple-darwin/release/libdefguard_boringtun.a \
+    -output target/universal/release/libdefguard_boringtun.a
 
 rm -f -r target/uniffi
 ${CARGO} run --release --bin uniffi-bindgen -- \
     --xcframework --headers --modulemap --swift-sources \
-    target/aarch64-apple-darwin/release/libboringtun.a target/uniffi
+    target/aarch64-apple-darwin/release/libdefguard_boringtun.a target/uniffi
 
 # Install BoringTun framework.
 
 mkdir -p "${DST}"
-cp -c target/uniffi/boringtun.swift "${DST}/"
-rm -f -r "${DST}/boringtun.xcframework"
+cp -c target/uniffi/defguard_boringtun.swift "${DST}/"
+rm -f -r "${DST}/defguard_boringtun.xcframework"
 xcodebuild -create-xcframework \
-    -library target/universal/release/libboringtun.a \
+    -library target/universal/release/libdefguard_boringtun.a \
     -headers target/uniffi \
-    -output ${DST}/boringtun.xcframework
-cp -c target/uniffi/boringtunFFI.h "${DST}/"
+    -output ${DST}/defguard_boringtun.xcframework
+cp -c target/uniffi/defguard_boringtunFFI.h "${DST}/"
 
 popd
 
