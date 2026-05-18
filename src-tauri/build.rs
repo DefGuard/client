fe4841b069eabd7eaf7ecc9fc38fd26b2283579e
Adam Ciarciński <adam@defguard.net>
2026-05-18 13:05:25 +0200

    Build new UI

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -3,6 +3,12 @@ use vergen_git2::{Emitter, Git2Builder};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-env-changed=DEFGUARD_CLIENT_BUILD_VERSION");
 
+    println!("cargo:rerun-if-env-changed=DEFGUARD_CLIENT_DEV");
+    println!("cargo::rustc-check-cfg=cfg(defguard_client_env)");
+    if std::env::var("DEFGUARD_CLIENT_DEV").is_ok() {
+        println!("cargo::rustc-cfg=defguard_client_env");
+    }
+
     // set VERGEN_GIT_SHA env variable based on git commit hash
     let git2 = Git2Builder::default().branch(true).sha(true).build()?;
     Emitter::default().add_instructions(&git2)?.emit()?;
