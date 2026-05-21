e9fae1060e22c26b46d0cd4c154c053a95ecd2b4
Kuba <78603704+jakub-tldr@users.noreply.github.com>
2026-05-21 12:54:42 +0200

    edit workflow

--- chocolatey/defguard/tools/chocolateyinstall.ps1
+++ chocolatey/defguard/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/DefGuard/client/releases/download/v1.6.8/Defguard_1.6.8_x64_en-US.msi'
-
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = '__MSI_URL__'
+$checksum = '__MSI_CHECKSUM__'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,8 @@ $packageArgs = @{
 
   softwareName  = 'defguard*'
 
-  checksum      = 'f7291e9d74cc270445bc1adc2624c2b74289f2276221f1c355f96d1db021871b'
+  checksum      = $checksum
   checksumType  = 'sha256'
-
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
@@ -21,3 +20,4 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 Write-Warning "IMPORTANT: Reboot or Re-login Required: On initial install the user is added to the defguard group.A reboot or logging out and back in is required for group membership changes to take effect. This is not required on subsequent updates."
+ 
