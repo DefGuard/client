3b5d0416e776e2749743db99b34d376915f96f54
Jacek Chmielewski <jacek@defguard.net>
2026-05-29 12:03:35 +0200

    loader component

--- new-ui/src/shared/components/LocationCard/views/LocationCardMfaEmailView/LocationCardMfaEmailView.tsx
+++ new-ui/src/shared/components/LocationCard/views/LocationCardMfaEmailView/LocationCardMfaEmailView.tsx
@@ -1,4 +1,3 @@
-import './style.scss';
 import { useCallback, useEffect, useState } from 'react';
 import { ThemeSpacing } from '../../../../types';
 import { isPresent } from '../../../../utils/isPresent';
@@ -10,13 +9,13 @@ import { Divider } from '../../../Divider/Divider';
 import { IconKind } from '../../../Icon';
 import { IconButton } from '../../../IconButton/IconButton';
 import { IconButtonVariant } from '../../../IconButton/types';
-import { LoaderSpinner } from '../../../LoaderSpinner/LoaderSpinner';
 import { SizedBox } from '../../../SizedBox/SizedBox';
 import { MfaStartMethod } from '../../api/startClientMfaSession';
 import { LocationViewHeader } from '../../components/LocationViewHeader/LocationViewHeader';
 import { useLocationCardContext } from '../../context/context';
 import { LocationCardViews } from '../../context/types';
 import { useMfaConnect } from '../../hooks/useMfaConnect';
+import { LocationCardMfaStartLoader } from '../LocationCardMfaStartLoader/LocationCardMfaStartLoader';
 
 export const LocationCardMfaEmailView = () => {
   const { setView, location } = useLocationCardContext();
@@ -52,15 +51,7 @@ export const LocationCardMfaEmailView = () => {
   // Show loader when posture is being evaluated
   const showLoader = location.posture_check_required && isStarting && !startError;
   if (showLoader) {
-    return (
-      <div className="mfa-start-loader">
-        <Divider spacing={ThemeSpacing.Md} />
-        <div className="loader-content">
-          <LoaderSpinner variant="primary" size={32} />
-          <p>Checking device requirements...</p>
-        </div>
-      </div>
-    );
+    return <LocationCardMfaStartLoader />;
   }
   return (
     <div
