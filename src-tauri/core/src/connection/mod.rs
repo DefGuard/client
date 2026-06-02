0109aeb75ee25693a2bc0df70427a6dfebc96e53
Maciej Wójcik <maciej@defguard.net>
2026-06-02 16:06:55 +0200

    setup unified connection interface

--- src-tauri/core/src/connection/mod.rs
+++ src-tauri/core/src/connection/mod.rs
@@ -3,3 +3,12 @@ pub mod setup;
 
 #[cfg(target_os = "macos")]
 pub mod apple;
+
+#[cfg(not(target_os = "macos"))]
+pub use setup::{disconnect_interface, execute_command, setup_interface, setup_interface_tunnel};
+
+#[cfg(target_os = "macos")]
+pub use apple::{
+    get_managers_for_tunnels_and_locations, location_tunnel_configuration,
+    sync_locations_and_tunnels, tunnel_stats, tunnel_tunnel_configuration, TunnelConfiguration,
+};
