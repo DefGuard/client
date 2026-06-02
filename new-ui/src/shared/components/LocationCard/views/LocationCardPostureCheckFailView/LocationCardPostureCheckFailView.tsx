a684d542e2e91cb88a57eab552fe0ccfc85132dc
Jacek Chmielewski <jchmielewski@teonite.com>
2026-06-02 13:19:15 +0200

    Fix posture errors icon and spacing (#931)

--- new-ui/src/shared/components/LocationCard/views/LocationCardPostureCheckFailView/LocationCardPostureCheckFailView.tsx
+++ new-ui/src/shared/components/LocationCard/views/LocationCardPostureCheckFailView/LocationCardPostureCheckFailView.tsx
@@ -1,13 +1,13 @@
 import './style.scss';
-import { ThemeSpacing, ThemeVariable } from '../../../../types';
+import { ThemeSpacing } from '../../../../types';
 import { Button } from '../../../Button/Button';
 import { ButtonVariant } from '../../../Button/types';
 import { Divider } from '../../../Divider/Divider';
-import { Icon, IconKind } from '../../../Icon';
 import { SizedBox } from '../../../SizedBox/SizedBox';
 import { LocationViewHeader } from '../../components/LocationViewHeader/LocationViewHeader';
 import { useLocationCardContext } from '../../context/context';
 import { LocationCardViews } from '../../context/types';
+import { NoConnectionIcon } from '../../images/NoConnectionIcon';
 
 export const LocationCardPostureCheckFailView = () => {
   const { postureError, setPostureError, setView } = useLocationCardContext();
@@ -27,13 +27,9 @@ export const LocationCardPostureCheckFailView = () => {
   return (
     <div className="location-card-posture-check-fail-view">
       <Divider spacing={ThemeSpacing.Md} />
-      <Icon
-        className="posture-error-icon"
-        icon={IconKind.GlobeBlocked}
-        size={48}
-        staticColor={ThemeVariable.FgWhite70}
-      />
-      <SizedBox height={ThemeSpacing.Md} />
+      <SizedBox height={ThemeSpacing.Xl} />
+      <NoConnectionIcon />
+      <SizedBox height={ThemeSpacing.Xl} />
       <LocationViewHeader title="Posture check failed">
         <div className="posture-errors">
           {postureErrors.map((error) => (
