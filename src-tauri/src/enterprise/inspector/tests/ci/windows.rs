2d0ef5150437907e39af35dbde1178637bc1ee0b
Jacek Chmielewski <jacek@defguard.net>
2026-06-09 09:42:45 +0200

    split windows tests into 2 setups

--- src-tauri/src/enterprise/inspector/tests/ci/windows.rs
+++ src-tauri/src/enterprise/inspector/tests/ci/windows.rs
@@ -3,57 +3,80 @@ use super::super::super::{
     security_update_age_days,
 };
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_os_name() {
-    assert_eq!(os_name().unwrap(), "Windows");
+mod setup1 {
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_os_name() {
+        assert_eq!(os_name().unwrap(), "Windows");
+    }
+
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_os_version() {
+        assert_eq!(os_version().unwrap(), "11");
+    }
+
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_anti_virus_status_on() {
+        assert_eq!(anti_virus_status().unwrap(), true);
+    }
+
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_part_of_domain_false() {
+        assert_eq!(part_of_domain().unwrap(), false);
+    }
+
+    // #[test]
+    // #[ignore = "CI posture testing only"]
+    // fn test_security_update_age_days() {
+    //     assert_eq!(security_update_age_days().unwrap(), 60);
+    // }
+
+
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_disk_encryption_status_unencrypted() {
+        assert_eq!(disk_encryption_status().unwrap(), false);
+    }
 }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_os_version() {
-    assert_eq!(os_version().unwrap(), "11");
-}
+mod setup2 {
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_os_name() {
+        assert_eq!(os_name().unwrap(), "Windows");
+    }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_anti_virus_status_off() {
-    assert_eq!(anti_virus_status().unwrap(), false);
-}
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_os_version() {
+        assert_eq!(os_version().unwrap(), "11");
+    }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_anti_virus_status_on() {
-    assert_eq!(anti_virus_status().unwrap(), true);
-}
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_anti_virus_status_off() {
+        assert_eq!(anti_virus_status().unwrap(), false);
+    }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_part_of_domain_false() {
-    assert_eq!(part_of_domain().unwrap(), false);
-}
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_part_of_domain_true() {
+        assert_eq!(part_of_domain().unwrap(), true);
+    }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_part_of_domain_true() {
-    assert_eq!(part_of_domain().unwrap(), true);
-}
+    // #[test]
+    // #[ignore = "CI posture testing only"]
+    // fn test_security_update_age_days() {
+    //     assert_eq!(security_update_age_days().unwrap(), 60);
+    // }
 
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_security_update_age_days() {
-    assert_eq!(security_update_age_days().unwrap(), 60);
-}
+    #[test]
+    #[ignore = "CI posture testing only"]
+    fn test_disk_encryption_status_encrypted() {
+        assert_eq!(disk_encryption_status().unwrap(), true);
+    }
 
-
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_disk_encryption_status_unencrypted() {
-    assert_eq!(disk_encryption_status().unwrap(), false);
-}
-
-#[test]
-#[ignore = "CI posture testing only"]
-fn test_disk_encryption_status_encrypted() {
-    assert_eq!(disk_encryption_status().unwrap(), true);
 }
