00806f6603067598b681c42a6cd35ec640a37da1
Filip Ślęzak <fslezak@teonite.com>
2026-06-10 14:01:19 +0200

    overview page selection

--- new-ui/src/pages/full/OverviewPage/OverviewPage.tsx
+++ new-ui/src/pages/full/OverviewPage/OverviewPage.tsx
@@ -1,3 +1,4 @@
+import './style.scss';
 import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { FullPage } from '../../../shared/layouts/FullPage/FullPage';
@@ -5,9 +6,10 @@ import { useAppData } from '../../../shared/providers/AppDataContext';
 import { getLocationsQueryOptions } from '../../../shared/rust-api/query';
 import { useAppStore } from '../../../shared/store/useAppStore';
 import { isPresent } from '../../../shared/utils/isPresent';
+import { OverviewSelection } from './components/OverviewSelection/OverviewSelection';
 
 export const OverviewPage = () => {
-  const { instances } = useAppData();
+  const { instances, tunnels } = useAppData();
   const selection = useAppStore((s) => s.compactViewSelection);
 
   const queryInstanceId = useMemo(() => {
@@ -18,7 +20,7 @@ export const OverviewPage = () => {
 
   const { data: locations } = useQuery(getLocationsQueryOptions(queryInstanceId));
 
-  const displayedLocations = useMemo(() => {
+  const _displayedLocations = useMemo(() => {
     if (!isPresent(selection) || selection.kind === 'instance') {
       return locations ?? [];
     }
@@ -28,30 +30,8 @@ export const OverviewPage = () => {
   return (
     <FullPage id="overview-page" hideScrollContainer>
       <div className="page-grid">
-        <div className="selection">
-          <div className="group">
-            <p>{`Instances`}</p>
-            <div className="items"></div>
-          </div>
-          <div className="group">
-            <p>{`Tunnels`}</p>
-            <div className="items"></div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h2>Instances</h2>
-        <ul>
-          {instances.map((instance) => (
-            <li key={instance.id}>{instance.name}</li>
-          ))}
-        </ul>
-        <h2>Locations / Tunnels</h2>
-        <ul>
-          {displayedLocations.map((location) => (
-            <li key={`${location.instance_id}-${location.id}`}>{location.name}</li>
-          ))}
-        </ul>
+        <OverviewSelection instances={instances} tunnels={tunnels} />
+        <div className="overview-content"></div>
       </div>
     </FullPage>
   );
