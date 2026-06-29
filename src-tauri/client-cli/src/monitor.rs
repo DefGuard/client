fe7bd649c225c00d195406b7be4eae453b4643f2
Jacek Chmielewski <jacek@defguard.net>
2026-06-29 10:36:08 +0200

    stale connection detection

--- src-tauri/client-cli/src/monitor.rs
+++ src-tauri/client-cli/src/monitor.rs
@@ -1,139 +1,50 @@
-use chrono::TimeDelta;
-use defguard_core::{ConnectionType, connection::active_connections::ACTIVE_CONNECTIONS};
+use chrono::{TimeDelta, Utc};
+use defguard_core::{
+    connection::{
+        active_connections::ACTIVE_CONNECTIONS,
+        active_state::{active_state, ActiveConnectionInfo},
+    },
+    ConnectionType,
+};
+use tracing::error;
 
 use crate::state::State;
 
-pub async fn monitor(state: &State) {
-    let connections = ACTIVE_CONNECTIONS.lock().await;
-    let connection_count = connections.len();
-    if connections.is_empty() {
-        return;
+fn is_stale(connection: &ActiveConnectionInfo, peer_alive_period: u32) -> Option<bool> {
+    let Some(stats) = connection.stats else {
+        return None;
+    };
+    let Some(last_handshake) = stats.last_handshake else {
+        return None;
     }
-    // TODO(jck): peer_alive_period from AppState
-    let peer_alive_period = TimeDelta::seconds(300);
-    // Check currently active connections.
-    for con in &*connections {
-        match con.connection_type {
-            ConnectionType::Location => {
-                // match LocationStats::latest_by_download_change(pool, con.location_id).await {
-                //     Ok(Some(latest_stat)) => {
-                //         trace!("Latest statistics for location: {latest_stat:?}");
-                //         if !check_last_active_connection(
-                //             latest_stat.collected_at,
-                //             peer_alive_period,
-                //         ) {
-                //             // Check if there was any traffic since the connection was established
-                //             // If not and the connection was established longer than the peer_alive_period,
-                //             // consider the location dead and disconnect it later without reconnecting.
-                //             let time_since_connection = Utc::now() - con.start.and_utc();
-                //             if latest_stat.collected_at < con.start {
-                //                 if time_since_connection > peer_alive_period {
-                //                     debug!(
-                //                       "There wasn't any activity for Location {} since its \
-                //                       connection at {}; considering it being dead and possibly \
-                //                       broken. It will be terminated without a further automatic \
-                //                       reconnect.",
-                //                       con.location_id, con.start
-                //                     );
-                //                     locations_to_disconnect.push((con.location_id, false));
-                //                 } else {
-                //                     debug!(
-                //                       "There wasn't any activity for Location {} since its \
-                //                       connection at {}; The amount of time passed since the connection \
-                //                       is {time_since_connection}, the connection will be terminated when it reaches \
-                //                       {peer_alive_period}",
-                //                     con.location_id, con.start);
-                //                 }
-                //             } else {
-                //                 debug!(
-                //                     "There wasn't any activity for Location {} for the last \
-                //                     {}s; considering it being dead.",
-                //                     con.location_id,
-                //                     peer_alive_period.num_seconds()
-                //                 );
-                //                 locations_to_disconnect.push((con.location_id, true));
-                //             }
-                //         }
-                //     }
-                //     Ok(None) => {
-                //         debug!(
-                //             "LocationStats not found in database for active connection {} {}({})",
-                //             con.connection_type, con.interface_name, con.location_id
-                //         );
-                //         if Utc::now() - con.start.and_utc() > peer_alive_period {
-                //             debug!(
-                //                 "There wasn't any activity for Location {} since its \
-                //                 connection at {}; considering it being dead.",
-                //                 con.location_id, con.start
-                //             );
-                //             locations_to_disconnect.push((con.location_id, false));
-                //         }
-                //     }
-                //     Err(err) => {
-                //         warn!(
-                //             "Verification for location {}({}) skipped due to database error: \
-                //             {err}",
-                //             con.interface_name, con.location_id
-                //         );
-                //     }
-                // }
+
+    let last_handshake = connection.stats?.last_handshake?;
+
+    Some(connection.stats?.last_handshake? > peer_alive_period as u64)
+}
+
+pub async fn monitor(state: &State) -> Result<(), defguard_core::error::Error> {
+    let connections = active_state(&state.pool).await?;
+    if connections.is_empty() {
+        return Ok(());
+    }
+    for connection in connections {
+        if is_stale(&connection, state.app_config.peer_alive_period).is_some_and(|v| v) {
+            let result;
+            #[cfg(not(target_os = "macos"))]
+            {
+                use defguard_core::connection::tear_down;
+
+                result = tear_down(&connection).await;
             }
-            ConnectionType::Tunnel => {
-                // match TunnelStats::latest_by_download_change(pool, con.location_id).await {
-                //     Ok(Some(latest_stat)) => {
-                //         trace!("Latest statistics for tunnel: {latest_stat:?}");
-                //         if !check_last_active_connection(
-                //             latest_stat.collected_at,
-                //             peer_alive_period,
-                //         ) {
-                //             // Check if there was any traffic since the connection was established.
-                //             // If not, consider the location dead and disconnect it later without reconnecting.
-                //             if latest_stat.collected_at - con.start < TimeDelta::zero() {
-                //                 debug!(
-                //                     "There wasn't any activity for Tunnel {} since its \
-                //                     connection at {}; considering it being dead and possibly \
-                //                     broken. It will be disconnected without a further \
-                //                     automatic reconnect.",
-                //                     con.location_id, con.start
-                //                 );
-                //                 tunnels_to_disconnect.push((con.location_id, false));
-                //             } else {
-                //                 debug!(
-                //                     "There wasn't any activity for Tunnel {} for the last
-                //                     {}s; considering it being dead.",
-                //                     con.location_id,
-                //                     peer_alive_period.num_seconds()
-                //                 );
-                //                 tunnels_to_disconnect.push((con.location_id, true));
-                //             }
-                //         }
-                //     }
-                //     Ok(None) => {
-                //         warn!(
-                //             "TunnelStats not found in database for active connection Tunnel {}({})",
-                //             con.interface_name, con.location_id
-                //         );
-                //         if Utc::now() - con.start.and_utc() > peer_alive_period {
-                //             debug!(
-                //                 "There wasn't any activity for Location {} since its \
-                //                 connection at {}; considering it being dead.",
-                //                 con.location_id, con.start
-                //             );
-                //             tunnels_to_disconnect.push((con.location_id, false));
-                //         }
-                //     }
-                //     Err(err) => {
-                //         warn!(
-                //             "Verification for tunnel {}({}) skipped due to db error. \
-                //             Error: {err}",
-                //             con.interface_name, con.location_id
-                //         );
-                //     }
-                // }
+            #[cfg(target_os = "macos")]
+            {
+                result = macos_tear_down(connection.clone()).await;
+            }
+            if let Err(err) = result {
+                error!("Error removing stale connection {}: {err}", connection.name);
             }
         }
     }
-    // Before processing locations/tunnels, the lock on active connections must be released.
-    drop(connections);
-
+    Ok(())
 }
