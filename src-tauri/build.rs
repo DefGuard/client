82dd3cd1cf80933f2f5f3c4721454c8a597849b3
Adam Ciarciński <adam@defguard.net>
2026-07-17 12:33:00 +0200

    Update wireguard-rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,4 +1,4 @@
-use vergen_git2::{Emitter, Git2Builder};
+use vergen_git2::{Emitter, Git2};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-env-changed=DEFGUARD_CLIENT_BUILD_VERSION");
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // set VERGEN_GIT_SHA env variable based on git commit hash
-    let git2 = Git2Builder::default().branch(true).sha(true).build()?;
+    let git2 = Git2::builder().branch(true).sha(true).build();
     Emitter::default().add_instructions(&git2)?.emit()?;
 
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "macos"
