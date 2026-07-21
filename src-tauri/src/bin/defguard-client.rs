9a1cb7cfa8fe9323207832c030dd58b18a78c4b5
Adam Ciarciński <adam@defguard.net>
2026-07-21 13:40:35 +0200

    Remove cli from packages

--- src-tauri/src/bin/defguard-client.rs
+++ src-tauri/src/bin/defguard-client.rs
@@ -1,5 +1,8 @@
 //! Defguard desktop client
 
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use std::env;
 
 use tauri::async_runtime;
