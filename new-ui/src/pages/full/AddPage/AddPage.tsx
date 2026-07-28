813c0cb7188b234bbd4af8dda6083fb784fde592
Maciej Wójcik <maciej@defguard.net>
2026-07-28 09:11:31 +0200

    hide Add tunnel card

--- new-ui/src/pages/full/AddPage/AddPage.tsx
+++ new-ui/src/pages/full/AddPage/AddPage.tsx
@@ -1,12 +1,16 @@
 import './style.scss';
+import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
 import { FullPageTitle } from '../../../shared/components/FullPageTitle/FullPageTitle';
 import { FullPage } from '../../../shared/layouts/FullPage/FullPage';
+import { getInstancesQueryOptions } from '../../../shared/rust-api/query';
 import { ThemeSpacing } from '../../../shared/types';
 import { AddCard } from './components/AddCard/AddCard';
 
 export const AddPage = () => {
   const navigate = useNavigate();
+  const { data: instances } = useQuery(getInstancesQueryOptions);
+  const tunnelsDisabled = instances?.some((i) => i.disable_tunnels) ?? false;
   return (
     <FullPage id="add-page-view">
       <FullPageTitle title="Add Defguard items" spacing={ThemeSpacing.Xl} />
@@ -22,17 +26,19 @@ export const AddPage = () => {
           actionText="Add instance"
           description={`Establish a secure connection to your Defguard instance effortlessly by configuring it with a single token—no manual setup.`}
         />
-        <AddCard
-          image="wireguard"
-          onClick={() => {
-            navigate({
-              to: '/full/add/tunnel',
-            });
-          }}
-          title="Add WireGuard Tunnel"
-          actionText="Add tunnel"
-          description={`Add and configure a WireGuard tunnel to securely route traffic through an encrypted connection using predefined configuration.`}
-        />
+        {!tunnelsDisabled && (
+          <AddCard
+            image="wireguard"
+            onClick={() => {
+              navigate({
+                to: '/full/add/tunnel',
+              });
+            }}
+            title="Add WireGuard Tunnel"
+            actionText="Add tunnel"
+            description={`Add and configure a WireGuard tunnel to securely route traffic through an encrypted connection using predefined configuration.`}
+          />
+        )}
       </div>
     </FullPage>
   );
